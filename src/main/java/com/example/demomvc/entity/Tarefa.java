7b8e0b4de5c23aefdcc92dd942c07ed0afff22c5
Jairo Duarte Ribeiro <jairoribeiro.dev@gmail.com>
2025-07-29 20:13:35 +0100

    Implement DAO for Tarefa

--- src/main/java/com/example/demomvc/entity/Tarefa.java
+++ src/main/java/com/example/demomvc/entity/Tarefa.java
@@ -4,24 +4,23 @@ import java.time.LocalDate;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
-public class Tarefa {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
-    private Long id;
+@Entity
+@Table(name = "tarefa")
+public class Tarefa extends AbstractEntity<Long> {
 
+    @Column(name = "nome", nullable = false, unique = true, length = 60)
     private String nome;
 
+    @Column(name = "data_entrega", nullable = false, columnDefinition = "DATE")
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate dataEntrega;
 
+    @Column(name = "responsavel", nullable = false, length = 60)
     private String responsavel;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getNome() {
         return nome;
@@ -45,55 +44,6 @@ public class Tarefa {
 
     public void setResponsavel(String responsavel) {
         this.responsavel = responsavel;
-    }
-
-    @Override
-    public String toString() {
-        return "Tarefa [id=" + id + ", nome=" + nome + ", dataEntrega=" + dataEntrega + ", responsavel=" + responsavel
-                + "]";
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
-        result = prime * result + ((dataEntrega == null) ? 0 : dataEntrega.hashCode());
-        result = prime * result + ((responsavel == null) ? 0 : responsavel.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Tarefa other = (Tarefa) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        if (nome == null) {
-            if (other.nome != null)
-                return false;
-        } else if (!nome.equals(other.nome))
-            return false;
-        if (dataEntrega == null) {
-            if (other.dataEntrega != null)
-                return false;
-        } else if (!dataEntrega.equals(other.dataEntrega))
-            return false;
-        if (responsavel == null) {
-            if (other.responsavel != null)
-                return false;
-        } else if (!responsavel.equals(other.responsavel))
-            return false;
-        return true;
     }
 
 }
