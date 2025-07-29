f9f3e8b552673b5d46f8877e103e7e71f9c3036c
Jairo Duarte Ribeiro <jairoribeiro.dev@gmail.com>
2025-07-29 18:49:14 +0100

    Create POJO Tarefa

---
+++ src/main/java/com/example/demomvc/entity/Tarefa.java
@@ -0,0 +1,99 @@
+package com.example.demomvc.entity;
+
+import java.time.LocalDate;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+public class Tarefa {
+
+    private Long id;
+
+    private String nome;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    private LocalDate dataEntrega;
+
+    private String responsavel;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataEntrega() {
+        return dataEntrega;
+    }
+
+    public void setDataEntrega(LocalDate dataEntrega) {
+        this.dataEntrega = dataEntrega;
+    }
+
+    public String getResponsavel() {
+        return responsavel;
+    }
+
+    public void setResponsavel(String responsavel) {
+        this.responsavel = responsavel;
+    }
+
+    @Override
+    public String toString() {
+        return "Tarefa [id=" + id + ", nome=" + nome + ", dataEntrega=" + dataEntrega + ", responsavel=" + responsavel
+                + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        result = prime * result + ((dataEntrega == null) ? 0 : dataEntrega.hashCode());
+        result = prime * result + ((responsavel == null) ? 0 : responsavel.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Tarefa other = (Tarefa) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        if (dataEntrega == null) {
+            if (other.dataEntrega != null)
+                return false;
+        } else if (!dataEntrega.equals(other.dataEntrega))
+            return false;
+        if (responsavel == null) {
+            if (other.responsavel != null)
+                return false;
+        } else if (!responsavel.equals(other.responsavel))
+            return false;
+        return true;
+    }
+
+}
